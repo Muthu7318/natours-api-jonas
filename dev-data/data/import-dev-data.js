2ab2327c97b75cf24d44fa0938978940faa6e4f9
muthukumar <muthumarshal@gmail.com>
2025-06-19 21:48:46 +0530

    import dev data to mongo db

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('./../../models/tourModel');
+
+dotenv.config({
+  path: './config.env',
+});
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.PASSWORD);
+
+mongoose.connect(DB).then(() => {
+  console.log('DB connection successful');
+});
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data succesfully loaded!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+const deleteAllData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data succesfully deleted!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteAllData();
+}
+
+console.log(process.argv);
